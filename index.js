9928163f7049693ef8d2f0c011cc797087a03653
Makuer <makuer.mqtur@student.moringaschool.com>
2025-03-02 12:43:25 +0300

    eventlistener

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    
+        const element = document.getElementById("button"); // Ensure your button has an id="button"
+        element.addEventListener("click", function() {
+            alert("Button was clicked!");
+        });
+    
+
 }
